c6799e5cae4b446f519c2f3b017c07a1a9de0e70
Kamil Ujas <kamilujas1@gmail.com>
2022-01-04 00:11:57 +0100

    app.zajavka.pl Day 6

---
+++ Day2/Horse.java
@@ -0,0 +1,16 @@
+package zajavka.Day2;
+
+public class Horse {
+
+    private String leftLegFront = "LLF";
+
+    private String rightLegFront = "RLF";
+
+    private String leftLegBack = "LLB";
+
+    private String rightLegBack = "RLB";
+
+    public String gimmeVoice() {
+        return "iaaa";
+    }
+}
---
+++ Day2/Pig.java
@@ -0,0 +1,17 @@
+package zajavka.Day2;
+
+public class Pig {
+
+    private String leftLegFront = "LLF";
+
+    private String rightLegFront = "RLF";
+
+    private String leftLegBack = "LLB";
+
+    private String rightLegBack = "RLB";
+
+    public String gimmeVoice() {
+        return "Muuu";
+    }
+
+}
